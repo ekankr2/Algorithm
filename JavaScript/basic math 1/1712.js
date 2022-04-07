cb6e0d913e089d912633315d85e1cf5050186f63
ekankr2 <ekankr2@naver.com>
2022-04-07 18:42:17 +0900

    1712 solved wrong

--- JavaScript/basic math 1/1712.js
+++ JavaScript/basic math 1/1712.js
@@ -1,24 +1,12 @@
 const fs = require('fs')
 const input = fs.readFileSync('input.txt').toString().trim().split(' ')
-let A = +input[0], B = +input[1], C = +input[2]
-let Answer;
+let 고정비용 = +input[0], 가변비용 = +input[1], 노트북가격 = +input[2]
 
-function 노트북매출 (고정비용, 가변비용, 판매량) {
-    return 고정비용 + 가변비용 * 판매량
-}
-
-function 손익분기 (노트북매출, 노트북가격) {
-    const 이익 = 노트북매출 - 노트북가격
-    return 이익 > 0;
-}
-
-while(true){
-    let i = 1
-    if(손익분기(노트북매출(A, B, i), C)){
-        Answer = i
-        break
+function 손익분기점(고정비용, 가변비용, 노트북가격) {
+    if(가변비용 > 노트북가격) {
+        return -1
     }
-    i++
+    return 고정비용 / (노트북가격 - 가변비용) + 1
 }
 
-console.log(Answer)
+console.log(손익분기점(고정비용, 가변비용, 노트북가격))
