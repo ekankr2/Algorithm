b5d40ec99f5cbf1e759777239b0c11508e0c5ed9
ekankr2 <ekankr2@naver.com>
2022-04-08 18:04:52 +0900

    1712 solved (math floor problem)

--- JavaScript/basic math 1/1712.js
+++ JavaScript/basic math 1/1712.js
@@ -3,10 +3,10 @@ const input = fs.readFileSync('input.txt').toString().trim().split(' ')
 let 고정비용 = +input[0], 가변비용 = +input[1], 노트북가격 = +input[2]
 
 function 손익분기점(고정비용, 가변비용, 노트북가격) {
-    if(가변비용 > 노트북가격) {
+    if((노트북가격 - 가변비용) <= 0) {
         return -1
     }
-    return 고정비용 / (노트북가격 - 가변비용) + 1
+    return Math.floor(고정비용 / (노트북가격 - 가변비용)) + 1
 }
 
 console.log(손익분기점(고정비용, 가변비용, 노트북가격))
