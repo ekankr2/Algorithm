5b38ce227bff9473e056e0327cc0c140bc3153db
ekankr2 <ekankr2@naver.com>
2022-05-04 18:41:20 +0900

    2839 try 1

---
+++ JavaScript/basic math 1/2839.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = +fs.readFileSync('input.txt').toString();
+
+const five = (input) => {
+    if(input % 5 % 3 > 0) {
+        return 0
+    }
+    return Math.floor(input / 5)
+}
+
+const three = (input) => {
+    if(five(input) === 0){
+        return input / 3
+    }
+    return input % 5 / 3
+}
+
+const solve = (input) => {
+    const count = five(input) + three(input)
+
+    if (count % 1 === 0) {
+        return count
+    }
+
+    return -1
+}
+
+console.log(solve(input))
