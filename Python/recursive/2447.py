0f96b4a165992c3a9f017ab56bb4bbd92576b2fc
ekankr2 <ekankr2@naver.com>
2022-07-21 18:59:20 +0900

    2447 try

--- Python/recursive/2447.py
+++ Python/recursive/2447.py
@@ -2,7 +2,14 @@ import math
 import sys
 
 N = int(sys.stdin.readline())
-
 세제곱근 = math.floor(27**(1/3))
 
-print(세제곱근)
+
+def recursive(n):
+    if  n**0.5
+
+    print(n * '*')
+    recursive(n-1)
+
+
+recursive()
