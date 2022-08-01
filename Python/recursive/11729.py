33ce91f94ce1b8158ec43e7c2e826f9caa6231f0
ekankr2 <ekankr2@naver.com>
2022-08-01 19:58:18 +0900

    11729 no solve

--- Python/recursive/11729.py
+++ Python/recursive/11729.py
@@ -1,5 +1,13 @@
-import sys
 
-N = int(sys.stdin.readline())
+def hanoi_tower(n, start, end) :
+    if n == 1 :
+        print(start, end)
+        return
 
-def hanoi():
+    hanoi_tower(n-1, start, 6-start-end) # 1단계
+    print(start, end) # 2단계
+    hanoi_tower(n-1, 6-start-end, end) # 3단계
+
+n = int(input())
+print(2**n-1)
+hanoi_tower(n, 1, 3)
