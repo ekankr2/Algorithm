a2557f9b3a54eb45857e70ca858ae96862f21df0
ekankr2 <ekankr2@naver.com>
2022-08-04 20:12:21 +0900

    2798 2

--- Python/brute force/2798.py
+++ Python/brute force/2798.py
@@ -1,11 +1,18 @@
 import sys
+from itertools import combinations
 
 N, M = map(int, sys.stdin.readline().split())
 num_list = list(map(int, sys.stdin.readline().split()))
-print(N, M)
-print(num_list)
+
 
 def black_jack(n, m, card_list):
+    closest_sum = 0
 
-    for i in card_list:
+    for i in combinations(card_list, 3):
+        temp_sum = sum(i)
+        if temp_sum == m:
+            closest_sum =
 
+
+
+black_jack(N, M, num_list)
