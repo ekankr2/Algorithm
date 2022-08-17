7db7f85ef82d392975596f6feedd15d6c1bd69ef
ekankr2 <ekankr2@naver.com>
2022-08-17 19:15:23 +0900

    1018 try2

--- Python/brute force/1018.py
+++ Python/brute force/1018.py
@@ -5,4 +5,10 @@ N, M = map(int, sys.stdin.readline().split())
 for i in range(N):
     print('')
     for j in range(M):
+        # print(j, end='')
+
+        if j % 2 == 1:
+            print('B', end='')
+            continue
+
         print('W', end='')
