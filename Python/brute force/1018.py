01e07a36a96aeeb5ac1d5104803308cb09f22679
ekankr2 <ekankr2@naver.com>
2022-08-24 19:37:28 +0900

    체스판 그리는 함수

--- Python/brute force/1018.py
+++ Python/brute force/1018.py
@@ -2,13 +2,26 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
+count = 0
+data = []
+def 체스판그리는녀석(i, j):
+    if i % 2 == 0:
+        if j % 2 == 0:
+            return 'W'
+
+        return 'B'
+
+    if j % 2 == 0:
+        return 'B'
+
+    return 'W'
+
+
 for i in range(N):
+    data = sys.stdin.readline().split()
+    print(data)
     print('')
     for j in range(M):
-        # print(j, end='')
-
-        if j % 2 == 1:
-            print('B', end='')
-            continue
-
-        print('W', end='')
+        print(j)
+        # print(data[j])
+        # print(체스판그리는녀석(i, j), end='')
